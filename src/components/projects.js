092608644e19cb0f6f932ddefe76ab5e1df75456
Aileen Liu <aileenliu184@gmail.com>
2019-09-29 15:41:26 -0700

    add flask project details

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, CardMenu, Button, IconButton } from 'react-mdl';
 
 class Projects extends Component {
     constructor(props) {
@@ -11,11 +11,28 @@ class Projects extends Component {
 
         if (this.state.activeTab === 0) {
             return (
-                <div><h1>This is Flask</h1></div>
+                <Card shadow={5} style={{minwidth: '450px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://miro.medium.com/max/438/1*0G5zu7CnXdMT9pGbYUTQLQ.png) center / cover'}}></CardTitle>
+                    <CardText>
+                        Instapic is a user-friendly social media web application. 
+                        It combines everything you may need for social media posting, 
+                        such as uploading photos with captions or hashtags, liking or 
+                        disliking a photo, saving a photo, making comments to photos 
+                        posted by other users, search photos by different hashtags, 
+                        just like a real Instagram.
+                    </CardText>
+                    <CardActions border>
+                        <Button href="https://github.com/xinbeiliu/Instapic-project"colored>GitHub</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
                 )
+
         } else if (this.state.activeTab === 1) {
             return (
-                <div><h1>This is React</h1></div>
+                <div><h1>Coming Soon...</h1></div>
                 )
         }
 
@@ -31,7 +48,11 @@ class Projects extends Component {
 
 
                 <section className="projects-grid">
-                    {this.toggleCategories()}
+                    <Grid className="project-grid">
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCategories()}</div>
+                        </Cell>
+                    </Grid>  
                 </section>
             </div>
             )
